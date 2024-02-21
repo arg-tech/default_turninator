35c080fc32a953e79c6738bc9087ec18fa902474
Debela Gemechu <debelagemechu@Debelas-MBP.wireless.dundee.ac.uk>
2024-02-21 10:47:34 +0000

    added PrometheusMetrics to track the end point using Garfana dashboards

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, request
-from turninator import Turninator 
-from utility import get_file
+from flask import Flask, request,jsonify
+from prometheus_flask_exporter import PrometheusMetrics
+
+
+from src.turninator import Turninator 
+from src.util import handle_errors
 import logging
 #logging configuration
 logging.basicConfig(datefmt='%H:%M:%S',
@@ -8,28 +11,29 @@ logging.basicConfig(datefmt='%H:%M:%S',
 
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 	
 @app.route('/turninator-01', methods = ['GET', 'POST'])
+@metrics.summary('requests_by_status', 'Request latencies by status',
+                 labels={'status': lambda r: r.status_code})
+@metrics.histogram('requests_by_status_and_path', 'Request latencies by status and path',
+                   labels={'status': lambda r: r.status_code, 'path': lambda: request.path})
+@handle_errors  
 def turninator_defult():
 	if request.method == 'POST':
-		print("posted")
-		file_obj = request.files['file']
-		f_name = get_file(file_obj)
-		
+		file_obj = request.files.get('file')		
 		turninator = Turninator()
-		result=turninator.turninator_default(f_name)
+		result=turninator.turninator_default(file_obj)
 		return result	
 
 	if request.method == 'GET':
-		info = """Turninanator is an AMF compononet that parses arguments into dialog turns.  
-		This is the default implmentation of a turninanator that parses arguments using a simple regular expressions. 
+		info = """Turninator is an AMF component that parses arguments into dialog turns.  
+		This is the default implementation of a turninator that parses arguments using simple regular expressions. 
 		It expects dialogical texts to be specified in the form of 'speaker:text' formats. 
-		It takes an input both in  text  and xIAF formats (where the input is specified in the text field of xIAF) to return xIAF as an output. 
-		Please note that the argument type (dialog vs monolog) can be specified in xIAF if the input is in xIAF format otherwise it is considered as monological argument. 
-		The component can be used as a starting point for creating an argument mining pipeline that takes monological or dialogical text specified as a regular text or xIAF."""
+		It takes input in both text and xIAF formats (where the input is specified in the text field of xIAF) to return xIAF as an output. 
+		Please note that the argument type (dialog vs monolog) can be specified in xIAF if the input is in xIAF format; otherwise, it is considered a monological argument. 
+		The component can be used as a starting point for creating an argument mining pipeline that takes monological or dialogical text specified as regular text or xIAF."""
 		return info
-
-
 	
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5006"), debug=False)	  
